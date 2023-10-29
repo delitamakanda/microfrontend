a08ba4341fbfd4637f6abde2f84e917f4997af1f
Délita Makanda <delita.makanda@gmail.com>
2023-10-30 00:28:05 +0100

    feat(breadcrumb): add breadcrumb #wip

--- adminpanel/src/components/breadcrumb/index.jsx
+++ adminpanel/src/components/breadcrumb/index.jsx
@@ -1,8 +1,21 @@
+import { BreadCrumb } from "primereact/breadcrumb";
+import { classNames } from "primereact/utils";
+import { Link } from "react-router-dom";
 
 export const Breadcrumb = () => {
+    const items = [].map((breadcrumb) => ({
+        label: breadcrumb.label,
+        to: breadcrumb.to,
+        template: (item, options) => {
+            return breadcrumb.href ? (<Link to={breadcrumb.href} className={classNames('text-color', options.className)}>
+                <i className={item.icon} />
+                <span className='ml-2'>{item.label}</span>
+            </Link>) : (<span>
+                <span className="p-2">{item.label}</span>
+            </span>)
+        }
+    }));
     return (
-        <div className="">
-            
-        </div>
+        items.length > 0 ? <BreadCrumb className="surface-ground pl-0 border-none" model={items} /> : null
     )
 }
