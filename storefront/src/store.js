7a29b61be3209c56e9d301125facf9cb32d545da
Délita Makanda <delita.makanda@gmail.com>
2023-11-19 00:56:43 +0100

    fix

--- storefront/src/store.js
+++ storefront/src/store.js
@@ -5,7 +5,7 @@ import { BASE_URL } from './constants';
 const useCount = () => useAtom(counterAtom);
 
 const articleData = atom(async () => {
-    const response = await fetch(`${BASE_URL}store/product/?ordering=-created_at`, {mode: 'no-cors', credentials: 'include', headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true', 'Access-Control-Allow-Methods': 'GET', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization' }});
+    const response = await fetch(`${BASE_URL}store/product/?ordering=-created_at`, {mode: 'no-cors', credentials: 'include', headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true', 'Access-Control-Allow-Methods': 'GET', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization', withCredentials: true }});
     const data = await response.json();
     return data;
 });
