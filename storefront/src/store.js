85683f3f80a3f13f35bd0c1ce7189f56a249ede0
Délita Makanda <delita.makanda@gmail.com>
2023-11-19 00:39:48 +0100

    allow origin

--- storefront/src/store.js
+++ storefront/src/store.js
@@ -5,7 +5,14 @@ import { BASE_URL } from './constants';
 const useCount = () => useAtom(counterAtom);
 
 const articleData = atom(async () => {
-    const response = await fetch(`${BASE_URL}store/product/?ordering=-created_at`);
+    const response = await fetch(`${BASE_URL}store/product/?ordering=-created_at`, {}, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'withCredentials': 'true',
+            'Access-Control-Allow-Origin': '*',
+        }
+    });
     const data = await response.json();
     return data;
 });
