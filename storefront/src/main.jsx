c7b835f390af34fa2a1372621243e71e282ab248
Délita Makanda <delitamakanda@users.noreply.github.com>
2023-11-21 15:22:55 +0100

    Update main.jsx

--- storefront/src/main.jsx
+++ storefront/src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import "primereact/resources/themes/tailwind-light/theme.css";
-import "primereact/resources/primereact.css";
-import "primeflex/primeflex.css";
-import "primeicons/primeicons.css";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
