e73e0bb0dd2f79b604655754ff5e8b13f47441ae
Délita Makanda <delitamakanda@users.noreply.github.com>
2023-11-23 09:56:44 +0100

    fix: ordering by most recent

--- adminpanel/src/pages/products/list.jsx
+++ adminpanel/src/pages/products/list.jsx
@@ -33,8 +33,8 @@ export const ProductList = () => {
         rows: 10,
         total: 0,
         page: 1,
-        sortField: null,
-        sortOrder: null
+        sortField: 'created_at',
+        sortOrder: '-'
     })
 
     let networkTimeout = null;
