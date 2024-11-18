d3e7ba32f88f6fba8dec9afbe43a7ee440a387d0
Délita Makanda <delitamakanda@users.noreply.github.com>
2024-11-18 14:17:34 +0100

    style style border

--- adminpanel/src/pages/login/index.jsx
+++ adminpanel/src/pages/login/index.jsx
@@ -91,7 +91,7 @@ export const Login = () => {
                     <InputText
                       id={field.name}
                       value={field.value}
-                      className={classNames("block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 ", {
+                      className={classNames("block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6 ", {
                         "p-invalid": fieldState.error,
                       })}
                       onChange={(e) => field.onChange(e.target.value)}
@@ -116,7 +116,7 @@ export const Login = () => {
                       id={field.name}
                       value={field.value}
                       type="password"
-                      className={classNames("block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6", {
+                      className={classNames("block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6", {
                         "p-invalid": fieldState.error,
                       })}
                       onChange={(e) => field.onChange(e.target.value)}
